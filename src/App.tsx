1dfaa8cc4c0d06a0e881261ba741e9549d86ba2b
maxim <fedaraumaxim@gmail.com>
2022-06-07 18:24:54 +0300

    Installed react-router + set up react router + configured folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+//Types
+import { ReactElement } from 'react';
 
-function App() {
-  return <div></div>;
+//React Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App(): ReactElement {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Main Page</h1>} />
+      <Route path="*" element={<h1>404</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
