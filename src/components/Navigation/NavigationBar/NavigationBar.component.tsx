d61f38820a50307510f1e0e2c6c36272e2db2072
maxim <fedaraumaxim@gmail.com>
2022-06-15 16:36:01 +0300

    Made Logo as Link to the main page

--- src/components/Navigation/NavigationBar/NavigationBar.component.tsx
+++ src/components/Navigation/NavigationBar/NavigationBar.component.tsx
@@ -12,14 +12,16 @@ import ButtonsContainer from '../ButtonsContainer/ButtonsContainer.component';
 import { ReactComponent as LogoSvg } from '../../../assets/images/logo.svg';
 
 //React Router
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function NavigationBar(): ReactElement {
   return (
     <>
       <NavigationContainer width="100%" height="72px">
         <NavigationContentContainer width="95%">
-          <LogoSvg width={95} height={45} />
+          <Link to="/">
+            <LogoSvg width={95} height={45} />
+          </Link>
           <ButtonsContainer />
         </NavigationContentContainer>
       </NavigationContainer>
