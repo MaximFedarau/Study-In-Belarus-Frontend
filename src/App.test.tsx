6adc0d147ad7a78ae9419c7b1d51b4ea17862476
maxim <fedaraumaxim@gmail.com>
2022-07-10 18:36:18 +0300

    Updated App.test.tsx: footer and header checking. Divided all pages into 2 categories: with header and footer and without 'em. And added check for 1 category.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-//RTL
-import { render } from '@testing-library/react';
-
-//Constants
+//Types
 import { PAGES_TITLES } from 'types/enum';
+
+//RTL
+import { render, screen } from '@testing-library/react';
 
 //Components
 import App from './App';
@@ -10,15 +10,15 @@ import App from './App';
 //React Navigation
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 
-describe('App has correct routing.', () => {
-  test('Correct 404 page Route.', () => {
-    render(
-      <MemoryRouter initialEntries={['/404']}>
-        <App />
-      </MemoryRouter>,
-    );
-    expect(document.title).toBe(PAGES_TITLES.NOT_FOUND);
+describe('Correct routing for pages with header and footer.', () => {
+  afterEach(() => {
+    // ! checking for at least one header and footer.
+    const headers = screen.getAllByRole('banner');
+    expect(headers.length).toBeGreaterThanOrEqual(1);
+    const footers = screen.getAllByRole('contentinfo');
+    expect(footers.length).toBeGreaterThanOrEqual(1);
   });
+
   test('Correct Home page Route.', () => {
     render(
       <BrowserRouter>
@@ -27,6 +27,7 @@ describe('App has correct routing.', () => {
     );
     expect(document.title).toBe(PAGES_TITLES.HOME);
   });
+
   test('Correct Advantages page Route.', () => {
     render(
       <MemoryRouter initialEntries={['/advantages']}>
@@ -34,5 +35,16 @@ describe('App has correct routing.', () => {
       </MemoryRouter>,
     );
     expect(document.title).toBe(PAGES_TITLES.ADVANTAGES_OF_STUDYING_IN_BELARUS);
+  });
+});
+
+describe('Correct routing for pages without header and footer.', () => {
+  test('Correct 404 page Route.', () => {
+    render(
+      <MemoryRouter initialEntries={['/404']}>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(document.title).toBe(PAGES_TITLES.NOT_FOUND);
   });
 });
